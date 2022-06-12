56782b9180c173e238567c7d167d5ec8542a7e74
Daetoun <97284852+Daetoun@users.noreply.github.com>
2022-06-13 00:15:42 +0100

    Complete Logic and View for the game

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,184 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import randomWords from "random-words";
+
+const MainComponent = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: #3a5369;
+  color: white;
+  /* overflow: hidden; */
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+  font-family: "MonteCarlo", cursive;
+  letter-spacing: 0.1em;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
 
 export default function Home() {
+  const NUMB_OF_WORDS = 200;
+  const SECONDS = 20;
+
+  const [words, setWords] = useState([]);
+  const [start, setStart] = useState(false);
+  const [countdown, setCountdown] = useState(SECONDS);
+  const [currentValue, setCurrentValue] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
+  const [completed, setCompleted] = useState(false);
+
+  const generateWords = () => {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
+  };
+
+  const countTimer = () => {
+    setStart(true);
+    setCompleted(false);
+    setCountdown(SECONDS);
+    setWords(generateWords());
+    setCorrect(0);
+    setWrong(0);
+    setCurrentIndex(0);
+
+    let interval = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev === 0) {
+          clearInterval(interval);
+          setStart(false);
+          setCompleted(true);
+          setCurrentValue("");
+          return SECONDS;
+        } else {
+          return prev - 1;
+        }
+      });
+    }, 1000);
+  };
+
+  console.log(completed);
+
+  function handleKeydown({ keyCode }) {
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrentValue("");
+      setCurrentIndex(currentIndex + 1);
+    }
+  }
+
+  const checkMatch = () => {
+    const wordsToCheck = words[currentIndex];
+    const isMatch = wordsToCheck === currentValue.trim();
+    if (isMatch) {
+      setCorrect(correct + 1);
+    } else {
+      setWrong(wrong + 1);
+    }
+  };
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Typing Speed game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <MainComponent className="bg-[#3a5369] w-full">
+        <section className="w-full max-h-screen">
+          <Title className="">Typing Test Game</Title>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          {/* timer */}
+          <div className="w-40 flex justify-center mx-auto items-center">
+            <div className="w-20 h-20 flex align-middle justify-center bg-[#243441] mx-auto mb-4 text-5xl">
+              <h1 className="m-auto font-black">{countdown}</h1>
+            </div>
+            <p className="text-lg px-2 font-bold">Seconds</p>
+          </div>
+          <div className="w-3/5 rounded-lg p-6 center bg-[#243441] shadow-md mx-auto">
+            {!start && (
+              <div className="mx-auto w-2/4 text-center">
+                {completed && (
+                  <>
+                    <h2 className="text-center text-3xl font-black underline mb-6">
+                      Scores
+                    </h2>
+                    <div className="flex justify-between mx-auto text-sm mb-8">
+                      <div className="px-6 py-4 border w-56">
+                        <p className="font-bold text-lg">Words per minute</p>
+                        <p className="italic text-base">{correct}</p>
+                      </div>
+                      <div className="px-6 py-4 border w-56">
+                        <p className="font-bold text-lg">Accuracy</p>
+                        <p className="italic text-base">
+                          {Math.round((correct / (correct + wrong)) * 100) || 0}{" "}
+                          %
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                )}
+                <p className="text-sm mb-4">
+                  <b className="text-lg underline">How to play: </b> <br />
+                  Start typing the words as soon as the timer start, the faster
+                  you are the higher your accuracy points.
+                </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <p className="text-lg mb-4">
+                  {completed
+                    ? "Click to restart the game"
+                    : "Click to Start the typing game"}
+                </p>
+                <button
+                  className="mx-auto rounded border w-32 text-sm p-2"
+                  onClick={countTimer}
+                >
+                  {completed ? "Restart" : "Start"}
+                </button>
+              </div>
+            )}
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            {start && (
+              <div>
+                {words.map((word, i) => (
+                  <span span key={i}>
+                    <span className="text-sm">
+                      {word.split("").map((char, indx) => (
+                        <span key={indx}>{char}</span>
+                      ))}
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            )}
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+          {start && (
+            <div className="w-3/4 mx-auto text-sm">
+              <input
+                type="text"
+                disabled={!start}
+                className="w-full p-2 mt-6 text-black outline-none rounded h-12"
+                placeholder="Type in here"
+                onKeyDown={handleKeydown}
+                value={currentValue}
+                onChange={(e) => setCurrentValue(e.target.value)}
+              />
+            </div>
+          )}
+        </section>
+      </MainComponent>
     </div>
-  )
+  );
 }
